edcd5e92d43e6836198e33305d83cfce8122be15
LifeLoggerAI <LifeLoggerAI@gmail.com>
2026-05-08 21:15:06 -0500

    Resolve narrator worker job type compatibly

--- workers/narrator-worker/src/handlers/index.ts
+++ workers/narrator-worker/src/handlers/index.ts
@@ -2,10 +2,12 @@ import { Job } from '@urai-jobs/shared-types';
 import { handleNarratorTts } from './narrator-tts.js';
 
 export async function handleJob(job: Job): Promise<any> {
-  switch (job.jobType) {
+  const jobType = job.type || job.jobType;
+
+  switch (jobType) {
     case 'narrator.tts':
       return handleNarratorTts(job);
     default:
-      throw new Error(`Unknown job type: ${job.jobType}`);
+      throw new Error(`Unknown job type: ${jobType || 'missing'}`);
   }
 }
